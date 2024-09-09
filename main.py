ea0fb6c294ba651177607b4b89085c8091dcf2b7
lighteko <huijunyi0212@naver.com>
2024-09-08 22:11:59 -0400

    .

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from crawler import Crawler
+from preprc import PreProcessor
 
 def main():
+    print("Welcom to DataBot V1.0. Please select the mode you would like to use:")
+    print("> 0: Crawler Mode")
+    print("> 1: Preprocessor Mode")
+    machine_mode = int(input("Enter the mode: "))
+    if machine_mode == 0:
+        crawler_mode()
+    elif machine_mode == 1:
+        preprocessor_mode()
+    else:
+        print("Invalid mode. Please try again.")
+
+def preprocessor_mode():
     print("""
-          WELCOME TO CRAWLER V1.0
+            Please select the mode you would like to use:
+            0: Google Play Store
+            1: Apple App Store
+            2: Reddit
+          """)
+    mode = int(input("Enter the mode: "))
+    p = PreProcessor(mode)
+    p.run()
+
+def crawler_mode():
+    print("""
             Please select the mode you would like to use:
             0: Google Play Store
             1: Apple App Store
--- preprc.py
+++ preprc.py
@@ -1,33 +1,314 @@
+import re
 import pandas as pd
+import gspread as gs
+import progressbar
+import string
+import nltk
+nltk.download()
+from collections import Counter
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet, stopwords
+from emos import Emos
+from spellchecker import SpellChecker
 
 class PreProcessor:
-    def __init__(self, file_name: str):
-        self.model: pd.DataFrame = pd.read_csv(f'./res/{file_name}.csv')
-
-    def remove_non_roman_character(self):
+    def __init__(self, mode: int):
+        print("Step 1: Initializing Preprocessor ...")
+        self.serv_acc = gs.service_account(filename='./credentials.json')
+        self.sheet = self.serv_acc.open("EST 205 Data Sheet")
+        self.file_name = input("Enter the worksheet name: ")
+        self.worksheet = self.sheet.worksheet(self.file_name)
+        self.model: pd.DataFrame = pd.DataFrame(self.worksheet.get_all_records())
+        self.STOPWORDS = stopwords.words('english')
+        self.PUNCTUATIONS = string.punctuation
+        counter = Counter()
+        bar = progressbar.ProgressBar(maxval=len(self.model)).start()
         for (_, row) in self.model.iterrows():
-            content = row['content']
-            for c in content:
-                if not (c <= 'z' or c >= 'a') and not (c <= 'Z' or c >= 'A'):
-                    content = content.replace(c, '')
+            bar.update(_)
+            for word in row['content'].split():
+                counter[word] += 1
+        self.FREQWORDS = set([w for (w, _) in counter.most_common(10)])
+        bar.finish()
+        self.RAREWORDS = set([w for (w, _) in counter.most_common()[:-10-1:-1]])
+        self.STEMMER = PorterStemmer().stem
+        self.LEMMATIZER = WordNetLemmatizer().lemmatize
+        self.WORDNET_MAP = {"N": wordnet.NOUN, "V": wordnet.VERB, "R": wordnet.ADV, "J": wordnet.ADJ}
+        self.EMOTICONS = Emos.EMOTICONS
+        self.UNICODE_EMO = Emos.UNICODE_EMO
+        self.SPELL = SpellChecker().correction
+    
+    def run(self):
+        print("Step 2: Preprocessing Data ...")
+        self.lowering()
+        self.remove_punctuation()
+        self.remove_stop_words()
+        self.remove_frequent_words()
+        self.remove_rare_words()
+        self.stemming()
+        self.lemmatizing()
+        self.convert_emojis()
+        self.convert_emoticons()
+        self.remove_urls()
+        self.remove_html_tags()
+        self.correct_spellings()
+        self.save()
+
+    def lowering(self):
+        print(">>> Lowering all characters ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": row['content'].lower(),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+            self.model.update
+        b.finish()
+        self.model = pd.DataFrame(temp_model)
+        print(self.model[:2])
+
+    def remove_punctuation(self):
+        print(">>> Removing punctuations ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": row['content'].translate(str.maketrans('', '', self.PUNCTUATIONS)),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        self.model = pd.DataFrame(temp_model)
+        b.finish()
 
     def remove_stop_words(self):
-        pass
+        print(">>> Removing stop words ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": ' '.join([word for word in row['content'].split() if word not in self.STOPWORDS]),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
 
-    def remove_html_tags(self):
+    def remove_frequent_words(self):
+        print(">>> Removing frequent words ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": ' '.join([word for word in row['content'].split() if word not in self.FREQWORDS]),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+    
+    def remove_rare_words(self):
+        print(">>> Removing rare words ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": ' '.join([word for word in row['content'].split() if word not in self.RAREWORDS]),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+
+    def stemming(self):
+        print(">>> Stemming words ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": ' '.join([self.STEMMER(word) for word in row['content'].split()]),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+        self.model = pd.DataFrame(temp_model)
+
+    def lemmatizing(self):
+        print(">>> Lemmatizing words ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            pos_tagged = nltk.pos_tag(row['content'].split())
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": ' '.join([self.LEMMATIZER(word, self.WORDNET_MAP.get(pos[0], wordnet.NOUN)) for word, pos in pos_tagged]),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+        self.model = pd.DataFrame(temp_model)
+
+    def convert_emojis(self):
+            
+        print(">>> Converting emojis ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
         for (_, row) in self.model.iterrows():
             content = row['content']
-            content = content.replace('<br>', '')
-            content = content.replace('<br/>', '')
-            content = content.replace('<br />', '')
-        pass
+            for emo in self.UNICODE_EMO:
+                content = re.sub(r'('+emo+')', "_".join(self.UNICODE_EMO[emo].replace(",","").replace(":","").split()), content)
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": content,
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+        self.model = pd.DataFrame(temp_model)
 
-    def remove_emojis(self):
-        pass
+    def convert_emoticons(self):
+        print(">>> Converting emoticons ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            content = row['content']
+            for emot in self.EMOTICONS:
+                content = re.sub(u'(' + emot + ')', "_".join(self.EMOTICONS[emot].replace(",","").split()), content)
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": content,
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+        self.model = pd.DataFrame(temp_model)
 
-    def remove_numbers(self):
-        pass
+    def remove_urls(self):
+        url_pattern = re.compile(r'https?://\S+|www\.\S+')
+        print(">>> Removing URLs ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": url_pattern.sub(r'', row['content']),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+        self.model = pd.DataFrame(temp_model)
 
-    def remove_extra_spaces(self):
-        pass
+    def remove_html_tags(self):
+        html_pattern = re.compile('<.*?>')
+        print(">>> Removing HTML tags ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": html_pattern.sub(r'', row['content']),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+        self.model = pd.DataFrame(temp_model)
 
+    def correct_spellings(self):
+        print(">>> Correcting spellings ...")
+        b = progressbar.ProgressBar(maxval=len(self.model)).start()
+        temp_model = []
+        
+        for (_, row) in self.model.iterrows():
+            b.update(_)
+            temp = {
+                "id": row['id'],
+                "date": row['date'],
+                "version": row['version'],
+                "score": row['score'],
+                "content": ' '.join([self.SPELL(word) for word in row['content'].split()]),
+                "upvote": row['upvote'],
+                "reply": row['reply'],
+                "reply_date": row['reply_date']
+            }
+            temp_model.append(temp)
+        b.finish()
+        self.model = pd.DataFrame(temp_model)
+
+    def save(self):
+        print("Step 3: Saving Preprocessed Data ...")
+        self.sheet.add_worksheet(title=f"Preprocessed_{self.file_name}")
+        worksheet = self.sheet.worksheet(f"Preprocessed_{self.file_name}")
+        worksheet.update([self.model.columns.values.tolist()] + self.model.values.tolist())
+        print(">>> Data saved successfully.")
+        print(">>> Exiting Preprocessor ...")
+        print(">>> Done.")
