6dde73de227b3c75e42368b65835407ad3feee5a
lighteko <huijunyi0212@naver.com>
2024-09-11 19:28:31 -0400

    .

--- preprc.py
+++ preprc.py
@@ -1,61 +1,81 @@
+from spellchecker import SpellChecker
+from emos import Emos
+from nltk.corpus import wordnet, stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+from collections import Counter
 import re
 import pandas as pd
 import gspread as gs
 import progressbar
 import string
 import nltk
-nltk.download()
-from collections import Counter
-from nltk.stem.porter import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet, stopwords
-from emos import Emos
-from spellchecker import SpellChecker
+# nltk.download()
+
 
 class PreProcessor:
     def __init__(self, mode: int):
-        print("Step 1: Initializing Preprocessor ...")
+        print("Step 0: Checking Credentials ...")
         self.serv_acc = gs.service_account(filename='./credentials.json')
         self.sheet = self.serv_acc.open("EST 205 Data Sheet")
-        self.file_name = input("Enter the worksheet name: ")
+        self.file_name = input(">>> Enter the worksheet name: ")
+        print(">>> Initializing Preprocessor ...")
         self.worksheet = self.sheet.worksheet(self.file_name)
-        self.model: pd.DataFrame = pd.DataFrame(self.worksheet.get_all_records())
+        self.model: pd.DataFrame = pd.DataFrame(
+            self.worksheet.get_all_records())
         self.STOPWORDS = stopwords.words('english')
         self.PUNCTUATIONS = string.punctuation
         counter = Counter()
-        bar = progressbar.ProgressBar(maxval=len(self.model)).start()
+        bar = progressbar.ProgressBar(maxval=len(self.model)+10).start()
         for (_, row) in self.model.iterrows():
             bar.update(_)
             for word in row['content'].split():
                 counter[word] += 1
         self.FREQWORDS = set([w for (w, _) in counter.most_common(10)])
-        bar.finish()
-        self.RAREWORDS = set([w for (w, _) in counter.most_common()[:-10-1:-1]])
+        self.RAREWORDS = set(
+            [w for (w, _) in counter.most_common()[:-10-1:-1]])
         self.STEMMER = PorterStemmer().stem
         self.LEMMATIZER = WordNetLemmatizer().lemmatize
-        self.WORDNET_MAP = {"N": wordnet.NOUN, "V": wordnet.VERB, "R": wordnet.ADV, "J": wordnet.ADJ}
+        self.WORDNET_MAP = {"N": wordnet.NOUN,
+                            "V": wordnet.VERB, "R": wordnet.ADV, "J": wordnet.ADJ}
         self.EMOTICONS = Emos.EMOTICONS
         self.UNICODE_EMO = Emos.UNICODE_EMO
-        self.SPELL = SpellChecker().correction
-    
+        self.CORRECTOR = SpellChecker().correction
+        self.UNKNOWN = SpellChecker().unknown
+        bar.update(len(self.model)+10)
+        bar.finish()
+
     def run(self):
         print("Step 2: Preprocessing Data ...")
+        print("[1 / 13]", end=" ")
         self.lowering()
+        print("[2 / 13]", end=" ")
         self.remove_punctuation()
+        print("[3 / 13]", end=" ")
         self.remove_stop_words()
+        print("[4 / 13]", end=" ")
         self.remove_frequent_words()
+        print("[5 / 13]", end=" ")
         self.remove_rare_words()
+        print("[6 / 13]", end=" ")
         self.stemming()
+        print("[7 / 13]", end=" ")
         self.lemmatizing()
+        print("[8 / 13]", end=" ")
         self.convert_emojis()
+        print("[9 / 13]", end=" ")
         self.convert_emoticons()
+        print("[10 / 13]", end=" ")
         self.remove_urls()
+        print("[11 / 13]", end=" ")
         self.remove_html_tags()
+        print("[12 / 13]", end=" ")
         self.correct_spellings()
+        print("[13 / 13]", end=" ")
         self.save()
 
     def lowering(self):
-        print(">>> Lowering all characters ...")
+        print("Lowering all characters ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -74,10 +94,9 @@ class PreProcessor:
             self.model.update
         b.finish()
         self.model = pd.DataFrame(temp_model)
-        print(self.model[:2])
 
     def remove_punctuation(self):
-        print(">>> Removing punctuations ...")
+        print("Removing punctuations ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -97,7 +116,7 @@ class PreProcessor:
         b.finish()
 
     def remove_stop_words(self):
-        print(">>> Removing stop words ...")
+        print("Removing stop words ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -116,7 +135,7 @@ class PreProcessor:
         b.finish()
 
     def remove_frequent_words(self):
-        print(">>> Removing frequent words ...")
+        print("Removing frequent words ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -133,9 +152,9 @@ class PreProcessor:
             }
             temp_model.append(temp)
         b.finish()
-    
+
     def remove_rare_words(self):
-        print(">>> Removing rare words ...")
+        print("Removing rare words ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -154,7 +173,7 @@ class PreProcessor:
         b.finish()
 
     def stemming(self):
-        print(">>> Stemming words ...")
+        print("Stemming words ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -174,7 +193,7 @@ class PreProcessor:
         self.model = pd.DataFrame(temp_model)
 
     def lemmatizing(self):
-        print(">>> Lemmatizing words ...")
+        print("Lemmatizing words ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -195,14 +214,15 @@ class PreProcessor:
         self.model = pd.DataFrame(temp_model)
 
     def convert_emojis(self):
-            
-        print(">>> Converting emojis ...")
+
+        print("Converting emojis ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
             content = row['content']
             for emo in self.UNICODE_EMO:
-                content = re.sub(r'('+emo+')', "_".join(self.UNICODE_EMO[emo].replace(",","").replace(":","").split()), content)
+                content = re.sub(
+                    r'('+emo+')', "_".join(self.UNICODE_EMO[emo].replace(",", "").replace(":", "").split()), content)
             b.update(_)
             temp = {
                 "id": row['id'],
@@ -219,13 +239,14 @@ class PreProcessor:
         self.model = pd.DataFrame(temp_model)
 
     def convert_emoticons(self):
-        print(">>> Converting emoticons ...")
+        print("Converting emoticons ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
             content = row['content']
             for emot in self.EMOTICONS:
-                content = re.sub(u'(' + emot + ')', "_".join(self.EMOTICONS[emot].replace(",","").split()), content)
+                content = re.sub(
+                    u'(' + emot + ')', "_".join(self.EMOTICONS[emot].replace(",", "").split()), content)
             b.update(_)
             temp = {
                 "id": row['id'],
@@ -243,7 +264,7 @@ class PreProcessor:
 
     def remove_urls(self):
         url_pattern = re.compile(r'https?://\S+|www\.\S+')
-        print(">>> Removing URLs ...")
+        print("Removing URLs ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -264,7 +285,7 @@ class PreProcessor:
 
     def remove_html_tags(self):
         html_pattern = re.compile('<.*?>')
-        print(">>> Removing HTML tags ...")
+        print("Removing HTML tags ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
         for (_, row) in self.model.iterrows():
@@ -284,18 +305,27 @@ class PreProcessor:
         self.model = pd.DataFrame(temp_model)
 
     def correct_spellings(self):
-        print(">>> Correcting spellings ...")
+        print("Correcting spellings ...")
         b = progressbar.ProgressBar(maxval=len(self.model)).start()
         temp_model = []
-        
         for (_, row) in self.model.iterrows():
             b.update(_)
+            content = row['content']
+            corrected_text = []
+            misspelled_words = self.UNKNOWN(content.split())
+            for word in content.split():
+                if word in misspelled_words:
+                    corrected_text.append(self.CORRECTOR(word))
+                    if corrected_text[-1] == None:
+                        corrected_text[-1] = word
+                else:
+                    corrected_text.append(word)
             temp = {
                 "id": row['id'],
                 "date": row['date'],
                 "version": row['version'],
                 "score": row['score'],
-                "content": ' '.join([self.SPELL(word) for word in row['content'].split()]),
+                "content": ' '.join(corrected_text),
                 "upvote": row['upvote'],
                 "reply": row['reply'],
                 "reply_date": row['reply_date']
@@ -306,9 +336,10 @@ class PreProcessor:
 
     def save(self):
         print("Step 3: Saving Preprocessed Data ...")
-        self.sheet.add_worksheet(title=f"Preprocessed_{self.file_name}")
+        self.sheet.add_worksheet(title=f"Preprocessed_{self.file_name}", rows=self.model.shape[0] + 1, cols=self.model.shape[1])
         worksheet = self.sheet.worksheet(f"Preprocessed_{self.file_name}")
-        worksheet.update([self.model.columns.values.tolist()] + self.model.values.tolist())
-        print(">>> Data saved successfully.")
+        worksheet.update([self.model.columns.values.tolist()
+                          ] + self.model.values.tolist())
+        print("Data saved successfully.")
         print(">>> Exiting Preprocessor ...")
-        print(">>> Done.")
+        print("DONE")
