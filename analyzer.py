1f0e8eda057ceb26b7b5dcd7a71655e03f69c688
lighteko <huijunyi0212@naver.com>
2024-10-23 16:38:13 -0400

    .

--- analyzer.py
+++ analyzer.py
@@ -5,6 +5,7 @@ import progressbar
 import numpy as np
 from api import EmotionsAPI as emAPI
 
+
 class Analyzer:
     def __init__(self):
         print("Step 0: Checking Credentials ...")
@@ -22,10 +23,10 @@ class Analyzer:
         self.VERSION_RATING = {}
         self.WORD_FREQ = []
         self.UPVOTEDS = None
-    
+
     def run(self):
         # self.anlz_emotion()
-        self.anlz_word_frequency()
+        # self.anlz_word_frequency()
         self.anlz_monthly_rating()
         self.anlz_rating_by_version()
         self.anlz_top5_upvoted_reviews()
@@ -76,7 +77,7 @@ class Analyzer:
         self.WORD_FREQ = [five, four, three, two, one]
         self.CUR_TASK += 1
         b.finish()
-        
+
     def anlz_monthly_rating(self) -> None:
         print(f"[{self.CUR_TASK}/{self.MAX_TASKS}] Analyzing Monthly Ratings ...")
         b = progressbar.ProgressBar(maxval=len(self.model) + 10).start()
@@ -87,7 +88,8 @@ class Analyzer:
                 self.MONTHLY_RATING[month] = []
             self.MONTHLY_RATING[month].append(int(row['score']))
         for month in self.MONTHLY_RATING:
-            self.MONTHLY_RATING[month] = np.mean(self.MONTHLY_RATING[month])
+            self.MONTHLY_RATING[month] = np.mean(
+                self.MONTHLY_RATING[month]).round(2)
         self.CUR_TASK += 1
         b.update(len(self.model) + 10)
         b.finish()
@@ -104,29 +106,52 @@ class Analyzer:
                 self.VERSION_RATING[version] = []
             self.VERSION_RATING[version].append(int(row['score']))
         for version in self.VERSION_RATING:
-            self.VERSION_RATING[version] = np.mean(self.VERSION_RATING[version])
+            self.VERSION_RATING[version] = np.mean(
+                self.VERSION_RATING[version]).round(2)
         self.CUR_TASK += 1
         b.update(len(self.model) + 10)
         b.finish()
 
     def anlz_top5_upvoted_reviews(self) -> None:
-        print(f"[{self.CUR_TASK}/{self.MAX_TASKS}] Analyzing Top 5 Upvoted Reviews ...")
-        self.UPVOTEDS = self.model.sort_values(by='upvote', ascending=False).head(5)
+        print(
+            f"[{self.CUR_TASK}/{self.MAX_TASKS}] Analyzing Top 5 Upvoted Reviews ...")
+        self.UPVOTEDS = self.model.sort_values(
+            by='upvote', ascending=False).head(5)
         self.CUR_TASK += 1
 
     def anlz_mentioning_competitors(self) -> None:
         pass
+    
+    def compare_versions(version1, version2) -> int:
+        for i in range(3):
+            if version1[i] > version2[i]:
+                return 1
+            elif version1[i] < version2[i]:
+                return -1
+        return 0
+
+    def sort_by_version(dataframe) -> None:
+        for (i, row) in dataframe.iterrows()[1:]:
+            version = row['version']
+            if version == None:
+                continue
+            version = version.split(".")
+            version = [int(x) for x in version]
+            
+            
 
     def save(self) -> None:
         print("Step 3: Saving Preprocessed Data ...")
-        # self.worksheet.update([self.model.columns.values.tolist()
-        #                        ] + self.model.values.tolist())
+        self.worksheet.update([self.model.columns.values.tolist()
+                               ] + self.model.values.tolist())
         pd.DataFrame(self.MONTHLY_RATING.items(), columns=[
-                     "Month", "Rating"]).to_csv(f"./res/analysis/{self.file_name.lower()}_monthly_rating.csv")
-        pd.DataFrame(self.VERSION_RATING.items(), columns=[
-                     "Version", "Rating"]).to_csv(f"./res/analysis/{self.file_name.lower()}_version_rating.csv")
-        pd.DataFrame(self.WORD_FREQ).to_csv(f"./res/analysis/{self.file_name.lower()}_word_frequency.csv")
-        pd.DataFrame(self.UPVOTEDS).to_csv(f"./res/analysis/{self.file_name.lower()}_top5_upvoted_reviews.csv")
+                     "Month", "Rating"]).to_csv(f"./res/analysis/{self.file_name.lower()}_monthly_rating.csv", index=False)
+        version_rating = pd.DataFrame(self.VERSION_RATING.items(), columns=[
+                     "Version", "Rating"])
+        version_rating.sort_index(inplace=True)
+        version_rating.to_csv(f"./res/analysis/{self.file_name.lower()}_version_rating.csv", index=False)
+        pd.DataFrame(self.UPVOTEDS).to_csv(
+            f"./res/analysis/{self.file_name.lower()}_top5_upvoted_reviews.csv", index=False)
         print("Data saved successfully.")
         print(">>> Exiting Preprocessor ...")
         print("DONE")
--- api.py
+++ api.py
@@ -1,21 +1,26 @@
 import requests
 import os
 
+
 class EmotionsAPI:
     def __init__(self):
         self.API_URL = "https://api-inference.huggingface.co/models/SamLowe/roberta-base-go_emotions"
-        self.headers = {"Authorization": f"Bearer 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"}
-    
+        self.headers = {
+            "Authorization": f"Bearer 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"}
+
     """
     returns top three probable emotions from the text
     """
-    def query(self, text: str)->list:
+
+    def query(self, text: str) -> list:
         payload = {"inputs": text}
-        response = requests.post(self.API_URL, headers=self.headers, json=payload)
+        response = requests.post(
+            self.API_URL, headers=self.headers, json=payload)
         try:
-            data = response.json()[0]
+            data = response.json()
+            result = [(obj['label'], obj['score']) for obj in data[0]]
+            print(result[:2])
         except:
+            print(response.text)
             return []
-        data = list(map(lambda x: (x['label'], x['score']), data))
-        return data[:2]
-    
+        return result[:2]
